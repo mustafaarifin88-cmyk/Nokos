d406107573854727d15b94ec689da5d5d753e0b3
Mustafa Arifin <mustafaarifin88@admin.sd.belajar.id>
2026-04-07 21:14:25 +0800

    Auto Save

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Models\ProdukModel;
+
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        $produkModel = new ProdukModel();
+        
+        $data['produk_terbaru'] = $produkModel->where('status', 'aktif')->orderBy('id', 'DESC')->findAll(8);
+        $data['produk_terlaris'] = $produkModel->where('status', 'aktif')->findAll(8);
+        
+        return view('front/home', $data);
     }
 }
