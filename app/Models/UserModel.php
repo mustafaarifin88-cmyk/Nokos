a58c4ff461b42648b4654a581bdbc2a6dda17af4
Mustafa Arifin <mustafaarifin88@admin.sd.belajar.id>
2026-04-07 20:49:35 +0800

    Auto Save

---
+++ app/Models/UserModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $allowedFields    = ['nama', 'username', 'password', 'role', 'foto'];
+    protected $useTimestamps    = false;
+}
